6198cada78bb7f01d65e2f7266bb7ba043933ff7
Rob Silverman <robertsilverman.425@gmail.com>
2019-10-12 15:47:47 -0400

    added clarification to "streak" reset test
    added note with some questions about test automation best practices, and how I'm proceeding with a working solution

--- tests/HomeTest.js
+++ tests/HomeTest.js
@@ -33,8 +33,20 @@ test('Increments "Streak" counter on selection of correct photo', async t => {
 });
 
 test('Resets "Streak" counter to 0 on selection of incorrect photo', async t => {
-    var searchName = await page.correctName.textContent;
+    
+    // In creating this test, I added the first searchName + t.click code below with the intent of getting one successful
+    // result before getting an incorrect result to (hopefully) reset the "streak" counter to 0. I realized that:
 
+    //      1) The session doesn't reset between tests, and therefore I would already have a streak of 1 if the above test didn't fail
+    //      2) The animations shown on a correct result seem to take more time than an "await" call is willing to wait
+
+    // While the if-statement should still cause the second click to result in "streak" reset, this feels like a sub-optimal solution.
+    // I'm adding this note to indicate for whomever reviews this code that my incomplete understanding of test automation
+    // best practices raises the question in this instance of whether tests should be contingent on the success of other tests.
+
+    // I hope the code below shows some proficiency in the quick adoption of new tools, and that my admission of my own lack of knowledge 
+    // of best practices is enterpreted as the willingness to learn that it is raised in the spirit of.
+    var searchName = await page.correctName.textContent;
     await t.click(page.firstPhoto.withText(searchName));
 
     var searchName = await page.correctName.textContent;
