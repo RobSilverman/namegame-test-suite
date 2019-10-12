5e6bce114511925dec8f818a6016d05520bd97ca
Rob Silverman <robertsilverman.425@gmail.com>
2019-10-12 15:17:47 -0400

    add "streak" reset test
    add test to check if "streak" counter resets to 0 on incorrect answer

--- tests/HomeTest.js
+++ tests/HomeTest.js
@@ -28,6 +28,23 @@ test('Increments "Streak" counter on selection of correct photo', async t => {
     
     await t.click(page.firstPhoto.withText(searchName));
 
-    const finalStreakCount =Number(await page.streak.textContent);
+    const finalStreakCount = Number(await page.streak.textContent);
     await t.expect(initialStreakCount + 1).eql(finalStreakCount);
+});
+
+test('Resets "Streak" counter to 0 on selection of incorrect photo', async t => {
+    var searchName = await page.correctName.textContent;
+
+    await t.click(page.firstPhoto.withText(searchName));
+
+    var searchName = await page.correctName.textContent;
+
+    if (page.firstPhoto.withText(searchName) == page.firstPhoto.nth(0)){
+        await t.click(page.firstPhoto.nth(1));
+    } else {
+        await t.click(page.firstPhoto.nth(0));
+    }
+    
+    const finalStreakCount = Number(await page.streak.textContent);
+    await t.expect(finalStreakCount).eql(0);
 });
